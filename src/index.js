dcf1bfad821d9fa115723055308ec7ac3d9438a8
Platane <me@platane.me>
2017-04-26 14:42:02 +0200

    :rocket: redux middle error log

--- src/index.js
+++ src/index.js
@@ -1,24 +1,51 @@
 require('file-loader?name=index.html!./index.html')
 
 import reducerTree              from './state'
-import * as Redux               from 'redux'
 import RefineryCreate           from 'refinery-js'
 import * as action              from 'action'
 
+import initUI                   from './sideEffect/ui'
+import initScheduler            from './sideEffect/scheduler'
 
-const { reduce, initState }   = RefineryCreate( reducerTree )
-
-const store = Redux.createStore( reduce, initState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
-
-;[
-    require('./sideEffect/ui'),
-    require('./sideEffect/scheduler'),
-]
-    .forEach( init => init( store ) )
+import { createStore, applyMiddleware, compose } from 'redux'
 
 
+const crashReporter = store => next => action => {
+    try {
+        return next(action)
+    } catch (err) {
+        console.error('Caught an exception!', err)
+        throw err
+    }
+}
 
+let store
 {
+
+    const { reduce, initState }   = RefineryCreate( reducerTree )
+
+    // create redux store
+    const middlewares = [
+        crashReporter,
+    ]
+    const enhancers = [
+        ...(
+            'undefined' != typeof window && window.__REDUX_DEVTOOLS_EXTENSION__
+            ? [ window.__REDUX_DEVTOOLS_EXTENSION__({ maxAge: 50, latency: 500 }) ]
+            : []
+        ),
+        applyMiddleware( ...middlewares ),
+    ]
+    store = createStore( reduce, initState, compose( ...enhancers ) )
+
+    ;[
+        initUI,
+        initScheduler,
+    ].forEach( init => init( store ) )
+
+
+
+
     const extractGeometry   = require('util/extractGeometry')
 
     const cubeOBJ   = require('raw-loader!./asset/obj/skull.obj')
